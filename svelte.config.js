2f134f6a63134d936c385ff9c63a8fa904eb8d32
TomFevrier <tomfevrier@gmail.com>
2022-08-26 15:23:53 +0200

    First commit

---
+++ svelte.config.js
@@ -0,0 +1,33 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import path from 'path';
+
+const dev = process.env.npm_lifecycle_event === 'dev';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+		scss: {
+			prependData: `@import './src/variables.scss';`
+		}
+	}),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			ssr: false,
+			trailingSlash: 'always'
+		}),
+		// alias: {
+		// 	'$components': path.resolve('./src/components'),
+		// 	'$utils': path.resolve('./src/utils'),
+		// 	'$stores': path.resolve('./src/stores')
+		// },
+		paths: {
+			base: dev ? '' : '',
+		}
+	},
+};
+
+export default config;
